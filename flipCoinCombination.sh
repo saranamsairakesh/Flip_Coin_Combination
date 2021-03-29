c09db30f26ca64b870c1268edbc18cdfdbc27d17
saranamsairakesh <saranamsairakesh@gmail.com>
2021-03-29 15:46:31 +0530

    Added: Singlet Dictionary

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,7 +1,26 @@
-flip=$(($RANDOM%2))
-if [ $flip -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+headsCount=0
+tailsCount=0
+declare -A singletDictionary
+function flipCoin(){
+   flip=$1
+   local result
+   if [ $flip -eq 1 ]
+   then
+      result="H"
+      headsCount=$(($headsCount+1))
+   else
+      result="T"
+      tailsCount=$(($tailsCount+1))
+   fi
+   echo $headsCount","$tailsCount","$result
+}
+
+for((i=1;i<=10;i++))
+do
+   flipResult="$( flipCoin $(($RANDOM%2)) )"
+   headsCount=$( echo $flipResult | awk -F , '{print $1}')
+   tailsCount=$( echo $flipResult | awk -F , '{print $2}')
+   singletDictionary[$i]=$( echo $flipResult | awk -F , '{print $3}')
+done
+
+echo ${singletDictionary[@]}
