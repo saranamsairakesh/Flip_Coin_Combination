14ddc7890fe611eb824672c1b11ae1fa1443f114
saranamsairakesh <saranamsairakesh@gmail.com>
2021-03-29 19:04:32 +0530

    Refactor: Doublet Dictionary modified

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,10 @@
-headsCount=0
-tailsCount=0
 declare -A singletDictionary
 declare -A doubletDictionary
+headsCount=0
+tailsCount=0
+
 function flipCoin(){
-   flip=$1
-   local result
-   if [ $flip -eq 1 ]
+   if [ $1 -eq 1 ]
    then
       result="H"
       headsCount=$(($headsCount+1))
@@ -16,26 +15,23 @@ function flipCoin(){
    echo $headsCount","$tailsCount","$result
 }
 
-doubleCount=0
-declare -A tempArray
-j=1
 for((i=1;i<=10;i++))
 do
    flipResult="$( flipCoin $(($RANDOM%2)) )"
    headsCount=$( echo $flipResult | awk -F , '{print $1}')
    tailsCount=$( echo $flipResult | awk -F , '{print $2}')
    singletDictionary[$i]=$( echo $flipResult | awk -F , '{print $3}')
-	tempArray[$doubleCount]=${singletDictionary[$i]}
-	doubleCount=$(($doubleCount+1))
-	if [ $doubleCount -eq 2 ]
-	then
-		doubleCount=0
-		doubletDictionary[$j]=${singletDictionary[$(($i-1))]}${singletDictionary[$i]}
-		j=$(($j+1))
-	fi
 done
 
-echo ${doubletDictionary[@]}
+for((i=1;i<=9;i++))
+do
+   doubletDictionary[$i]=${singletDictionary[$i]}${singletDictionary[$(($i+1))]}
+done
+
+percentageOfHeads=$(($headsCount*100/10))
+percentageofTails=$(($tailsCount*100/10))
+echo "Percentage of Heads= "$percentageOfHeads"%"
+echo "Percentage of Tails= "$percentageofTails"%"
 
 HHCount=0
 HTCount=0
@@ -48,23 +44,24 @@ do
 			("HH")
 				HHCount=$(($HHCount+1))
 				;;
-         ("HT")
-            HTCount=$(($HTCount+1))
-            ;;
-         ("TH")
-            THCount=$(($THCount+1))
-            ;;
-         ("TT")
-            TTCount=$(($TTCount+1))
-            ;;
+			("HT")
+				HTCount=$(($HTCount+1))
+				;;
+			("TH")
+				THCount=$(($THCount+1))
+				;;
+			("TT")
+				TTCount=$(($TTCount+1))
+				;;
 	esac
 done
 
-percentageOfHH=$(($HHCount*200/10))
-percentageofHT=$(($HTCount*200/10))
-percentageOfTH=$(($THCount*200/10))
-percentageofTT=$(($TTCount*200/10))
+percentageOfHH=$(($HHCount*100/9))
+percentageofHT=$(($HTCount*100/9))
+percentageOfTH=$(($THCount*100/9))
+percentageofTT=$(($TTCount*100/9))
 echo "percentage Of HH= "$percentageOfHH"%"
 echo "percentage Of HT= "$percentageofHT"%"
 echo "percentage Of TH= "$percentageOfTH"%"
 echo "percentage Of TT= "$percentageofTT"%"
+
